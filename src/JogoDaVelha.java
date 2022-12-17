2be3a1065c2f6bb9631e04a327092582eee86482
Rodrigo Veras <rodrigo.veras37@gmail.com>
2022-12-17 15:25:03 -0300

    Criado Método para verificar se uma jogada é possível e criados métodos para cada jogador fazer sua jogada

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class JogoDaVelha {
 
     public static void imprimirTabuleiro (Character[][] tabuleiro){
@@ -21,6 +23,48 @@ public class JogoDaVelha {
             System.out.println();
         }
         System.out.println("---------------");
+    }
+    private static boolean jogadaValida (Character[][] tabuleiro, int[] jogada){
+        int linhaEscolhida = jogada [0];
+        int colunaEscolhida = jogada [1];
+
+        if (linhaEscolhida < 1 || linhaEscolhida > 3){
+            return false;
+        }
+        if (colunaEscolhida < 1 || colunaEscolhida > 3){
+            return false;
+        }
+        if (tabuleiro[linhaEscolhida-1][colunaEscolhida-1] != null){
+            return false;
+        }
+        return true;
+    }
+    private static int [] jogarSimbolo (Character[][] tabuleiro, Scanner scanner){
+        int [] jogada = new int[2];
+
+        jogada [0] = scanner.nextInt();
+        jogada [1] = scanner.nextInt();
+
+        while (!jogadaValida(tabuleiro, jogada)){
+            System.out.println("Jogada inválida! tente novamente:");
+            jogada [0] = scanner.nextInt();
+            jogada [1] = scanner.nextInt();
+        }
+        return jogada;
+    }
+    public static void jogadaDeX (Character[][] tabuleiro, Scanner scanner){
+        System.out.println("Jogador 1 (X) faça sua jogada:");
+
+        int [] jogadaX = jogarSimbolo(tabuleiro, scanner);
+
+        tabuleiro[jogadaX[0]-1][jogadaX[1]-1] = 'X';
+    }
+    public static void jogadaDeO (Character[][] tabuleiro, Scanner scanner){
+        System.out.println("Jogador 2 (O) faça sua jogada:");
+
+        int [] jogadaO = jogarSimbolo(tabuleiro, scanner);
+
+        tabuleiro[jogadaO[0]-1][jogadaO[1]-1] = 'O';
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Character [][] tabuleiro = new Character[3][3];
+        Scanner scanner = new Scanner(System.in);
 
         JogoDaVelha.imprimirTabuleiro(tabuleiro);
+
+        JogoDaVelha.jogadaDeX(tabuleiro, scanner);
+        JogoDaVelha.imprimirTabuleiro(tabuleiro);
+
+        JogoDaVelha.jogadaDeO(tabuleiro, scanner);
+        JogoDaVelha.imprimirTabuleiro(tabuleiro);
+
+        JogoDaVelha.jogadaDeX(tabuleiro, scanner);
+        JogoDaVelha.imprimirTabuleiro(tabuleiro);
+
+        JogoDaVelha.jogadaDeO(tabuleiro, scanner);
+        JogoDaVelha.imprimirTabuleiro(tabuleiro);
+
+
     }
 }
